ccc0b206db6eb5d97492433233ef7fa5bd7d4c0f
Fatima Zahrae Ben El Hachmi <fzbenelhachmi@SMB.SQLI.COM>
2021-03-25 15:22:20 +0100

    first commit

--- src/composants/Blog.js
+++ src/composants/Blog.js
@@ -2,9 +2,49 @@ import React, { Component } from 'react'
 import NvPost from './NvPost/NvPost'
 import PostModale from './PostModale/PostModale'
 import './Blog.css'
+import axios from 'axios'
+import Post from './Post/Post'
 class Blog extends Component {
+    state={
+        posts: [],
+        selectPostId: null,
+        toggle:false //pour le bouton fermer
+    }
+componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response=>{
+        const article =response.data.slice(0,4)
+        const postAuteur =article.map(post=>{
+            return {
+                ...post,
+                auteur:'Huge'
+            }
+        })
+this.setState({
+    posts:postAuteur
+})
+    })
+}
+selectId = id =>{
+this.setState({
+    selectPostId:id,
+})
+this.setState({
+    toggle:true, //si on a clique sur un araticle
+})
+}
+//fonction pour bouton fermer
+toggeModele=()=>{
+    this.setState({toggle:false})
+}
 
     render () {
+        const posts =this.state.posts.map(e=>{
+            return <Post auteur={e.auteur}
+             key={e.id} titre={e.title}
+             clicked={() => this.selectId(e.id)}
+             />
+        })
 
         return (
             <div>
@@ -12,8 +52,12 @@ class Blog extends Component {
                 <NvPost />
                 </section>
                 <h2 className="text-center my-5">Choisissez un post ...</h2>
-                <PostModale />
+                <PostModale 
+                cache={this.toggeModele}
+                toggle={this.state.toggle}
+                id={this.state.selectPostId}  />
                 <section className="Posts">
+                    {posts}
                 </section>
 
             </div>
--- src/composants/PostModale/PostModale.js
+++ src/composants/PostModale/PostModale.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 import './PostModale.css'
 
 class PostModale extends Component {
+    state={
+        loadPost :null
+    }
+componentDidUpdate(){
+ //   console.log('update')
+ if(this.props.id){
+     //pour éviter boucle infiner
+     if(!this.state.loadPost || (this.state.loadPost && this.state.loadPost.id !== this.props.id)){
+        axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+        .then(respose=>{
+           // console.log(respose)
+           this.setState({
+               loadPost:respose.data
+           })
+        })
+     }
+    
+ }
+}
 
     render () {
 
         return (
+            this.state.loadPost && this.props.toggle?
                 <div className="PostComplet">
-                    <h1>Titre</h1>
-                    <p>Contenu</p>
+                    <h1>{this.state.loadPost.title}</h1>
+                    <p> {this.state.loadPost.body}</p>
             
-                    <button className="btn btn-danger my-3 btnPost">Fermer</button>
+                    <button className="btn btn-danger my-3 btnPost"
+                    onClick={this.props.cache}
+                    >Fermer</button>
                 
                 </div>
-            
+            :null
             )
         
     
